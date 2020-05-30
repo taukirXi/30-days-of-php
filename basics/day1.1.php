5ede0c877573c36897e2a93860fcd386ff03b126
taukir <taukirahmed779@gmail.com>
2020-05-31 00:32:26 +0600

    php basics day1

---
+++ basics/day1.1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // php is whitespace insenitive
+    $capital = 67;
+    echo "variable capital is $capital<br>";
+    // echo "variable capital is $CaPiTaL<br>";
+
+
+    // printing in php
+    $greeting = "welcome to php<br>";
+    
+    print($greeting);
+
+
+    // some basics block
+    if (3 == 2+1){
+        echo "good - i haven't totally lost my mind.<br>";
+    }
+
+
+    ?>
+
+</body>
+</html>
---
+++ basics/day1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    # this is a comment, and
+    # this is the second line of comment.
+
+    // this is a comment too, each style comments only.
+    echo "an example with line comments";
+    ?>
+
+    
+</body>
+</html>
