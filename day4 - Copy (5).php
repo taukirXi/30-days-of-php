0a33aa51ac1114079e5a4c2a61ad29d2e32f4751
taukir <taukirahmed779@gmail.com>
2020-06-02 23:23:47 +0600

    day 4 done.

---
+++ day4 - Copy (5).php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <?php
+    // showing text in a browser
+    //excecise 5................
+    
+    $month = date('m');
+
+    if ($month == 8){
+        echo "it's august, so it's really hot. <br>";
+    } else {
+        echo "not august, so at least no in the peak of the heat.<br>";
+    }
+
+
+    ?>
+
+
+
+</body>
+</html>
