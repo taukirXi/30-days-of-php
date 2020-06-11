692fa98a3610c1f7382cb17fe703b2f30de2f5a4
taukir <taukirahmed779@gmail.com>
2020-06-12 00:44:21 +0600

    some changed  | modified

--- guess.php
+++ guess.php
@@ -6,15 +6,15 @@
 <h1>Welcome to my guessing game</h1>
 <p>
 <?php
-  if ( ! isset($_GET['guess']) ) { 
+  if ( ! isset($_POST['guess']) ) { 
     echo("Missing guess parameter");
-  } else if ( strlen($_GET['guess']) < 1 ) {
+  } else if ( strlen($_POST['guess']) < 1 ) {
     echo("Your guess is too short");
-  } else if ( ! is_numeric($_GET['guess']) ) {
+  } else if ( ! is_numeric($_POST['guess']) ) {
     echo("Your guess is not a number");
-  } else if ( $_GET['guess'] < 99 ) {
+  } else if ( $_POST['guess'] < 99 ) {
     echo("Your guess is too low");
-  } else if ( $_GET['guess'] > 99 ) {
+  } else if ( $_POST['guess'] > 99 ) {
     echo("Your guess is too high");
   } else {
     echo("Congratulations - You are right");
---
+++ guessGame.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>guessgame.v.1 </title>
+</head>
+<body>
+    <h2>Welcome to my guessing Game</h2>
+
+    <form action="guess.php" method='post'>
+    
+    input your guess : <input type="text" name='guess'><br>
+    <input type="submit"><br>
+    
+    
+    
+    
+    
+    
+    
+    </form>
+</body>
+</html>
+
+
